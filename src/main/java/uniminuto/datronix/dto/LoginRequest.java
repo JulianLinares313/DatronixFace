0e11b38e914bbb494137b2fa33b89c154315e354
JulinLinares313 <julian.linares-s@uniminuto.edu.co>
2026-09-24 13:29:03 -0500

    Nuevos cambio

--- src/main/java/uniminuto/datronix/dto/LoginRequest.java
+++ src/main/java/uniminuto/datronix/dto/LoginRequest.java
@@ -1,12 +1,19 @@
 package uniminuto.datronix.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 // Agrupa el correo y la contraseña que llegan desde el formulario de acceso.
 public class LoginRequest {
 
+    
+    @NotBlank(message = "El correo es obligatorio")
+    @Email(message = "El correo debe tener un formato válido")
     private String correoUsuario;
+
+    @NotBlank(message = "La comtraseña es obligatoriaG")
     private String contrasenaUsuario;
 
 }
